065863bf8c1bd2897bdb8197c014f225e9dd8e32
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-05-31 01:40:51 +0100

    sprawozdanie RK4 stały krok

--- RK4_staly_krok.m
+++ RK4_staly_krok.m
@@ -7,9 +7,7 @@ function [ x, T, errx, t ] = RK4_staly_krok( dx, x0, przedzial, krok )
 %   T - interwa³y przedzia³u (potrzebne do wykresu itp)
 %   errx - b³¹d rozwi¹zania
 %   t - czas obliczeñ
-
 tic;
-
 yn=x0;%y0
 h=krok;
 h2=0.5*h;
@@ -19,7 +17,6 @@ errx=zeros(length(przedzial(1):krok:przedzial(2)),2);
 T=zeros(length(przedzial(1):krok:przedzial(2)),1);
 x(j,:)=yn';
 T(j)=0;
-
 for i=przedzial(1):krok:przedzial(2)
     j=j+1;
     k1=dx(yn);%zmiana oznaczeñ w celu zgodnoœci z podrêcznikiem
@@ -27,20 +24,18 @@ for i=przedzial(1):krok:przedzial(2)
     k3=dx(yn+0.5*h*k2);
     k4=dx(yn+h*k3);
     ynp1=yn+1/6*h*(k1+2*k2+2*k3+k4);
-    x(j,:)=yn';
+    x(j,:)=ynp1';
     for i2=1:2%obliczenie yn+1 w dwóch o po³owê mniejszych krokach na potrzeby oszacownaia b³êdu
         k1=dx(yn);
-        k2=dx(yn+0.5*h*k1);
-        k3=dx(yn+0.5*h*k2);
-        k4=dx(yn+h*k3);
-        yn=yn+1/6*h*(k1+2*k2+2*k3+k4);
+        k2=dx(yn+0.5*h2*k1);
+        k3=dx(yn+0.5*h2*k2);
+        k4=dx(yn+h2*k3);
+        yn=yn+1/6*h2*(k1+2*k2+2*k3+k4);
     end
-    errx(j,:)=16/15*(yn-ynp1);
+    errx(j,:)=abs(16/15*(yn-ynp1));
     yn=ynp1;
     T(j)=i+h;
 end
-
-
 t=toc;
 end
 
--- Zadanie4_2.m
+++ Zadanie4_2.m
@@ -2,6 +2,7 @@ function [ output_args ] = Zadanie4_2( input_args )
 %Zadanie4_2 rozwi¹zanie zadania z projektu 4
 %   Detailed explanation goes here
 
+%% a
 dx = @(x) [  x(2)+x(1)*(0.9-x(1)^2-x(2)^2) , ...
             -x(1)+x(2)*(0.9-x(1)^2-x(2)^2) ];
 
@@ -16,12 +17,82 @@ x0a=[10, 8];
 figure;
 plot(T,Y);
 
-krok=0.0146583;
+%krok=0.0146583;
+%krok=0.0146582;
+krok=0.001;
                 
 [ x, Tx, errx, t ]=RK4_staly_krok(dx, x0a, przedzial, krok);
 
 figure;
 plot(Tx,x);
+
+figure;
+plot(Tx, errx);
+
+t
+
+%% b
+
+x0b=[0, 9];
+
+[T,Y]=ode45(@(t,x)dx(x)', przedzial, x0b);
+
+figure;
+plot(T,Y);
+
+krok=0.0146582;
+                
+[ x, Tx, errx, t ]=RK4_staly_krok(dx, x0b, przedzial, krok);
+
+figure;
+plot(Tx,x);
+
+figure;
+plot(Tx, errx);
+
+t
+
+%% c
+
+x0c=[8, 0];
+
+[T,Y]=ode45(@(t,x)dx(x)', przedzial, x0c);
+
+figure;
+plot(T,Y);
+
+krok=0.038;
+                
+[ x, Tx, errx, t ]=RK4_staly_krok(dx, x0c, przedzial, krok);
+
+figure;
+plot(Tx,x);
+
+figure;
+plot(Tx, errx);
+
+t
+
+%% d
+
+x0d=[0.001, 0.001];
+
+[T,Y]=ode45(@(t,x)dx(x)', przedzial, x0d);
+
+figure;
+plot(T,Y);
+
+krok=0.2;
+                
+[ x, Tx, errx, t ]=RK4_staly_krok(dx, x0d, przedzial, krok);
+
+figure;
+plot(Tx,x);
+
+figure;
+plot(Tx, errx);
+
+t
 
 end
 
